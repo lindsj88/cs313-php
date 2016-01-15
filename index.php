11878fdd4e1f0c9b28d99b7c2c1b6015e416c855
lindsj88 <oak06003@byui.edu>
2016-01-15 16:41:54 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<div class="menu">  
 			<link href="/style.css" type="text/css" rel="stylesheet" />
 			<h2>CS 313 - Lindsey Moeller</h2>
-			<img alt="logo" src="/Untitled-1.jpg" id="logo">
+			<img alt="logo" src="/Untitled-1.JPG" id="logo">
      		<ul>
                 <li><a href="/index.php">Home</a></li>
                 <li><a href="/assignments.php">Assignments</a></li>

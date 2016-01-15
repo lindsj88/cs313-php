19da4e58efb20972689db40e9d66f5c2b85c2b3e
Lindsey Moeller <Lindsey Moeller>
2016-01-15 16:15:52 -0700

    made home page and assignment page for week 2

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8">
+        <title>Assignments | CS313</title>
+    </head>
+<header>
+		<div class="menu">  
+			<link href="/style.css" type="text/css" rel="stylesheet" />
+			<h2>CS 313 - Lindsey Moeller</h2>
+			<img alt="logo" src="/Untitled-1.jpg" id="logo">
+     		<ul>
+                <li><a href="/index.php">Home</a></li>
+                <li><a href="/about.php">Assignments</a></li>
+			</ul>
+		</div>
+	</header>
+    
+       
+    <main id="intro">
+        <h3>Assignments</h3>
+		<p>This page coming soon!</p>
+	</main>
+    <footer>
+ 		Copyright &copy; Lindsey Moeller <?php print date("2015"); ?>
+    </footer>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home | CS313</title>
+    </head>
+<header>
+		<div class="menu">  
+			<link href="/style.css" type="text/css" rel="stylesheet" />
+			<h2>CS 313 - Lindsey Moeller</h2>
+			<img alt="logo" src="/Untitled-1.jpg" id="logo">
+     		<ul>
+                <li><a href="/index.php">Home</a></li>
+                <li><a href="/about.php">Assignments</a></li>
+			</ul>
+		</div>
+	</header>
+    
+       
+    <main id="intro">
+        <h3>Home</h3>
+		<img class="run" alt="push 5k" src="/IMG_0487.jpg">
+		<p>This is the new home page for my CS 313 site.</p>
+			<ul>
+				<li>My name is Lindsey</li>
+				<li>I am a web design and development major</li>
+				<li>I will graduate next semester</li>
+				<li>In my spare (yeah right) time I like to run and sew</li>
+				<li>I don't like milk</li>
+			</ul>
+	</main>
+    <footer>
+ 		Copyright &copy; Lindsey Moeller <?php print date("2015"); ?>
+    </footer>
+</html>
